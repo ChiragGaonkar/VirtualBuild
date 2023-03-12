21205e94980662309fb1d19e71ba0eb2b53392f0
gayatriigaonkar <gayugaonkar52@gmail.com>
2023-03-13 01:12:30 +0530

    sendmessageboxwidget

---
+++ lib/widgets/sendmessagebox.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SendMessageBox extends StatefulWidget {
+  const SendMessageBox({super.key});
+
+  @override
+  State<SendMessageBox> createState() => _SendMessageBoxState();
+}
+
+class _SendMessageBoxState extends State<SendMessageBox> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      splashColor: Theme.of(context).primaryColor,
+      //code for
+    );
+  }
+}
